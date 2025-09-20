a566d45e2f0d45a6a56fd917f6b0b07b7d9ac16e
Sean Crain <38871906+seancrain@users.noreply.github.com>
2025-09-20 09:20:48 -0500

    Implement all connected types

--- src/main/java/com/nosiphus/furniture/block/ModernBedBlock.java
+++ src/main/java/com/nosiphus/furniture/block/ModernBedBlock.java
@@ -12,6 +12,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -43,20 +44,62 @@ public class ModernBedBlock extends FurnitureHorizontalBlock
 
         //Single Bed - Back
         final VoxelShape[] SINGLE_BACK_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 4.5, 0.0, 15.0, 5.5, 14.0), Direction.SOUTH));
-        final VoxelShape[] SINGLE_BACK_RIGHT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.0, 0.0, 1.0, 8.0, 14.0), Direction.SOUTH));
-        final VoxelShape[] SINGLE_BACK_LEFT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(15.0, 4.0, 0.0, 16.0, 8.0, 14.0), Direction.SOUTH));
-        final VoxelShape[] SINGLE_BACK_FRONTBOARD = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 14.0, 16.0, 7.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] SINGLE_BACK_LEFT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.0, 0.0, 1.0, 8.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] SINGLE_BACK_RIGHT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(15.0, 4.0, 0.0, 16.0, 8.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] SINGLE_BACK_FOOT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 14.0, 16.0, 7.0, 16.0), Direction.SOUTH));
         final VoxelShape[] SINGLE_BACK_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 5.0, 0.0, 15.0, 9.0, 14.0), Direction.SOUTH));
         final VoxelShape[] SINGLE_BACK_FRONT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 7.0, 14.0, 16.0, 8.0, 15.0), Direction.SOUTH));
 
         //Single Bed - Head
         final VoxelShape[] SINGLE_HEAD_HEADBOARD = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 0.0, 16.0, 15.0, 2.0), Direction.SOUTH));
         final VoxelShape[] SINGLE_HEAD_HEADBOARD_TOP = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 15.0, 0.0, 15.0, 16.0, 2.0), Direction.SOUTH));
-        final VoxelShape[] SINGLE_HEAD_RIGHT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.0, 2.0, 1.0, 8.0, 16.0), Direction.SOUTH));
-        final VoxelShape[] SINGLE_HEAD_LEFT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(15.0, 4.0, 2.0, 16.0, 8.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] SINGLE_HEAD_LEFT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.0, 2.0, 1.0, 8.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] SINGLE_HEAD_RIGHT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(15.0, 4.0, 2.0, 16.0, 8.0, 16.0), Direction.SOUTH));
         final VoxelShape[] SINGLE_HEAD_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 4.5, 2.0, 15.0, 5.5, 16.0), Direction.SOUTH));
         final VoxelShape[] SINGLE_HEAD_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 5.0, 8.0, 15.0, 9.0, 16.0), Direction.SOUTH));
         final VoxelShape[] SINGLE_HEAD_PILLOW = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 5.0, 2.0, 15.0, 9.5, 8.0), Direction.SOUTH));
+
+        //Left Bed - Back
+        final VoxelShape[] LEFT_BACK_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 4.5, 0.0, 16.0, 5.5, 15.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_BACK_LEFT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.0, 0.0, 1.0, 8.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_BACK_FOOT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 14.0, 16.0, 7.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_BACK_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 5.0, 0.0, 16.0, 9.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_BACK_FRONT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 7.0, 14.0, 16.0, 8.0, 15.0), Direction.SOUTH));
+
+        //Left Bed - Head
+        final VoxelShape[] LEFT_HEAD_HEADBOARD = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 0.0, 16.0, 15.0, 2.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_HEAD_HEADBOARD_TOP = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 15.0, 0.0, 16.0, 16.0, 2.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_HEAD_LEFT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.0, 2.0, 1.0, 8.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_HEAD_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 4.5, 2.0, 16.0, 5.5, 16.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_HEAD_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 5.0, 8.0, 16.0, 9.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] LEFT_HEAD_PILLOW = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(1.0, 5.0, 2.0, 16.0, 9.5, 8.0), Direction.SOUTH));
+
+        //Right Bed - Back
+        final VoxelShape[] RIGHT_BACK_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.5, 0.0, 15.0, 5.5, 14.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_BACK_RIGHT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(15.0, 4.0, 0.0, 16.0, 8.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_BACK_FOOT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 14.0, 16.0, 7.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_BACK_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 5.0, 0.0, 15.0, 9.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_BACK_FRONT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 7.0, 14.0, 16.0, 8.0, 15.0), Direction.SOUTH));
+
+        //Right Bed - Head
+        final VoxelShape[] RIGHT_HEAD_HEADBOARD = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 0.0, 16.0, 15.0, 2.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_HEAD_HEADBOARD_TOP = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 15.0, 0.0, 15.0, 16.0, 2.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_HEAD_RIGHT_SIDE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(15.0, 4.0, 2.0, 16.0, 8.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_HEAD_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.5, 2.0, 15.0, 5.5, 16.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_HEAD_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 5.0, 8.0, 15.0, 9.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] RIGHT_HEAD_PILLOW = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 5.0, 2.0, 15.0, 9.5, 8.0), Direction.SOUTH));
+
+        //Middle Bed - Back
+        final VoxelShape[] MIDDLE_BACK_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.5, 0.0, 16.0, 5.5, 14.0), Direction.SOUTH));
+        final VoxelShape[] MIDDLE_BACK_FOOT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 14.0, 16.0, 7.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] MIDDLE_BACK_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 5.0, 0.0, 16.0, 9.0, 14.0), Direction.SOUTH));
+        final VoxelShape[] MIDDLE_BACK_FRONT = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 7.0, 14.0, 16.0, 8.0, 15.0), Direction.SOUTH));
+
+        //Middle Bed - Head
+        final VoxelShape[] MIDDLE_HEAD_HEADBOARD = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 2.0), Direction.SOUTH));
+        final VoxelShape[] MIDDLE_HEAD_BASE = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 4.5, 2.0, 16.0, 5.5, 16.0), Direction.SOUTH));
+        final VoxelShape[] MIDDLE_HEAD_BLANKET = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 5.0, 8.0, 16.0, 9.0, 16.0), Direction.SOUTH));
+        final VoxelShape[] MIDDLE_HEAD_PILLOW = VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.box(0.0, 5.0, 2.0, 16.0, 9.5, 8.0), Direction.SOUTH));
 
         ImmutableMap.Builder<BlockState, VoxelShape> builder = new ImmutableMap.Builder<>();
         for (BlockState state : states) {
@@ -70,17 +113,17 @@ public class ModernBedBlock extends FurnitureHorizontalBlock
                     switch(type) {
                         case BACK:
                             shapes.add(SINGLE_BACK_BASE[direction.get2DDataValue()]);
-                            shapes.add(SINGLE_BACK_RIGHT_SIDE[direction.get2DDataValue()]);
                             shapes.add(SINGLE_BACK_LEFT_SIDE[direction.get2DDataValue()]);
-                            shapes.add(SINGLE_BACK_FRONTBOARD[direction.get2DDataValue()]);
+                            shapes.add(SINGLE_BACK_RIGHT_SIDE[direction.get2DDataValue()]);
+                            shapes.add(SINGLE_BACK_FOOT[direction.get2DDataValue()]);
                             shapes.add(SINGLE_BACK_BLANKET[direction.get2DDataValue()]);
                             shapes.add(SINGLE_BACK_FRONT[direction.get2DDataValue()]);
                             break;
                         case HEAD:
                             shapes.add(SINGLE_HEAD_HEADBOARD[direction.get2DDataValue()]);
                             shapes.add(SINGLE_HEAD_HEADBOARD_TOP[direction.get2DDataValue()]);
-                            shapes.add(SINGLE_HEAD_RIGHT_SIDE[direction.get2DDataValue()]);
                             shapes.add(SINGLE_HEAD_LEFT_SIDE[direction.get2DDataValue()]);
+                            shapes.add(SINGLE_HEAD_RIGHT_SIDE[direction.get2DDataValue()]);
                             shapes.add(SINGLE_HEAD_BASE[direction.get2DDataValue()]);
                             shapes.add(SINGLE_HEAD_BLANKET[direction.get2DDataValue()]);
                             shapes.add(SINGLE_HEAD_PILLOW[direction.get2DDataValue()]);
@@ -88,10 +131,58 @@ public class ModernBedBlock extends FurnitureHorizontalBlock
                     }
                     break;
                 case LEFT:
+                    switch(type) {
+                        case BACK:
+                            shapes.add(LEFT_BACK_BASE[direction.get2DDataValue()]);
+                            shapes.add(LEFT_BACK_LEFT_SIDE[direction.get2DDataValue()]);
+                            shapes.add(LEFT_BACK_FOOT[direction.get2DDataValue()]);
+                            shapes.add(LEFT_BACK_BLANKET[direction.get2DDataValue()]);
+                            shapes.add(LEFT_BACK_FRONT[direction.get2DDataValue()]);
+                            break;
+                        case HEAD:
+                            shapes.add(LEFT_HEAD_HEADBOARD[direction.get2DDataValue()]);
+                            shapes.add(LEFT_HEAD_HEADBOARD_TOP[direction.get2DDataValue()]);
+                            shapes.add(LEFT_HEAD_LEFT_SIDE[direction.get2DDataValue()]);
+                            shapes.add(LEFT_HEAD_BASE[direction.get2DDataValue()]);
+                            shapes.add(LEFT_HEAD_BLANKET[direction.get2DDataValue()]);
+                            shapes.add(LEFT_HEAD_PILLOW[direction.get2DDataValue()]);
+                            break;
+                    }
                     break;
                 case RIGHT:
+                    switch(type) {
+                        case BACK:
+                            shapes.add(RIGHT_BACK_BASE[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_BACK_RIGHT_SIDE[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_BACK_FOOT[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_BACK_BLANKET[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_BACK_FRONT[direction.get2DDataValue()]);
+                            break;
+                        case HEAD:
+                            shapes.add(RIGHT_HEAD_HEADBOARD[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_HEAD_HEADBOARD_TOP[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_HEAD_RIGHT_SIDE[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_HEAD_BASE[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_HEAD_BLANKET[direction.get2DDataValue()]);
+                            shapes.add(RIGHT_HEAD_PILLOW[direction.get2DDataValue()]);
+                            break;
+                    }
                     break;
-                case BOTH:
+                case MIDDLE:
+                    switch(type) {
+                        case BACK:
+                            shapes.add(MIDDLE_BACK_BASE[direction.get2DDataValue()]);
+                            shapes.add(MIDDLE_BACK_FOOT[direction.get2DDataValue()]);
+                            shapes.add(MIDDLE_BACK_BLANKET[direction.get2DDataValue()]);
+                            shapes.add(MIDDLE_BACK_FRONT[direction.get2DDataValue()]);
+                            break;
+                        case HEAD:
+                            shapes.add(MIDDLE_HEAD_HEADBOARD[direction.get2DDataValue()]);
+                            shapes.add(MIDDLE_HEAD_BASE[direction.get2DDataValue()]);
+                            shapes.add(MIDDLE_HEAD_BLANKET[direction.get2DDataValue()]);
+                            shapes.add(MIDDLE_HEAD_PILLOW[direction.get2DDataValue()]);
+                            break;
+                    }
                     break;
             }
 
@@ -122,7 +213,8 @@ public class ModernBedBlock extends FurnitureHorizontalBlock
         Level level = context.getLevel();
         BlockPos headPos = context.getClickedPos().relative(direction);
         if(level.getBlockState(headPos).canBeReplaced(context) && level.getWorldBorder().isWithinBounds(headPos)) {
-            return this.defaultBlockState().setValue(DIRECTION, direction);
+            BlockState state = this.defaultBlockState().setValue(DIRECTION, direction);
+            return this.getModernBedState(state, context.getLevel(), context.getClickedPos(), state.getValue(DIRECTION));
         }
         return null;
     }
@@ -156,6 +248,34 @@ public class ModernBedBlock extends FurnitureHorizontalBlock
     }
 
     @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState newState, LevelAccessor level, BlockPos pos, BlockPos newPos) {
+        return this.getModernBedState(state, level, pos, state.getValue(DIRECTION));
+    }
+
+    private BlockState getModernBedState(BlockState state, LevelAccessor level, BlockPos pos, Direction dir) {
+        boolean left = this.isModernBed(level, pos, dir.getCounterClockWise(), dir) || this.isModernBed(level, pos, dir.getCounterClockWise(), dir.getCounterClockWise());
+        boolean right = this.isModernBed(level, pos, dir.getClockWise(), dir) || this.isModernBed(level, pos, dir.getClockWise(), dir.getClockWise());
+
+        if(left && right) {
+            return state.setValue(CONNECTED, Connected.MIDDLE);
+        } else if (left) {
+            return state.setValue(CONNECTED, Connected.RIGHT);
+        } else if (right) {
+            return state.setValue(CONNECTED, Connected.LEFT);
+        }
+        return state.setValue(CONNECTED, Connected.SINGLE);
+    }
+
+    private boolean isModernBed(LevelAccessor level, BlockPos source, Direction direction, Direction targetDirection) {
+        BlockState state = level.getBlockState(source.relative(direction));
+        if(state.getBlock() == this) {
+            Direction bedDirection = state.getValue(DIRECTION);
+            return bedDirection.equals(targetDirection);
+        }
+        return false;
+    }
+
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
         super.createBlockStateDefinition(builder);
@@ -167,7 +287,7 @@ public class ModernBedBlock extends FurnitureHorizontalBlock
         SINGLE("single"),
         LEFT("left"),
         RIGHT("right"),
-        BOTH("both");
+        MIDDLE("middle");
 
         private final String name;
 
