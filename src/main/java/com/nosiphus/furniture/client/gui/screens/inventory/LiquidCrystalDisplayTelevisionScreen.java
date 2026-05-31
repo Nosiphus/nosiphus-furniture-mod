f42f297df3fd925dde2e71ff57383a62fbb9891c
Gerald S <gerald@meappy.com>
2026-05-31 04:32:49 +0000

    feat: add LCD TV screen

---
+++ src/main/java/com/nosiphus/furniture/client/gui/screens/inventory/LiquidCrystalDisplayTelevisionScreen.java
@@ -0,0 +1,111 @@
+package com.nosiphus.furniture.client.gui.screens.inventory;
+
+import com.nosiphus.furniture.network.protocol.common.ServerboundLcdTvUpdate;
+import com.nosiphus.furniture.world.inventory.LiquidCrystalDisplayTelevisionMenu;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+public class LiquidCrystalDisplayTelevisionScreen extends AbstractContainerScreen<LiquidCrystalDisplayTelevisionMenu> {
+
+    // GUI dimensions - no texture needed, purely programmatic
+    private static final int GUI_WIDTH = 176;
+    private static final int GUI_HEIGHT = 120;
+
+    // Colors for channel buttons
+    private static final int COLOR_RED    = 0xFFAA0000;
+    private static final int COLOR_GREEN  = 0xFF00AA00;
+    private static final int COLOR_BLUE   = 0xFF0000AA;
+    private static final int COLOR_YELLOW = 0xFFAAAA00;
+
+    // Channel names
+    private static final String[] CHANNEL_NAMES = {"Ch 1", "Ch 2", "Ch 3", "Ch 4"};
+    private static final int[] CHANNEL_COLORS = {
+        0xFFFF5555, // Red
+        0xFF55FF55, // Green
+        0xFF5555FF, // Blue
+        0xFFFFFF55  // Yellow
+    };
+
+    public LiquidCrystalDisplayTelevisionScreen(LiquidCrystalDisplayTelevisionMenu menu, Inventory inventory, Component component) {
+        super(menu, inventory, component);
+        this.imageWidth = GUI_WIDTH;
+        this.imageHeight = GUI_HEIGHT;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        int startX = this.leftPos;
+        int startY = this.topPos;
+
+        // Power toggle button (top center)
+        this.addRenderableWidget(Button.builder(getPowerLabel(), btn -> {
+            boolean newPower = !this.menu.isPowered();
+            int channel = this.menu.getChannel();
+            PacketDistributor.sendToServer(new ServerboundLcdTvUpdate(
+                    this.menu.getBlockEntity().getBlockPos(), newPower, channel));
+            btn.setMessage(newPower
+                    ? Component.literal("Power: ON")
+                    : Component.literal("Power: OFF"));
+        }).bounds(startX + 63, startY + 10, 50, 20).build());
+
+        // Channel buttons (in a row below power)
+        for (int i = 0; i < 4; i++) {
+            final int channelIndex = i;
+            this.addRenderableWidget(Button.builder(Component.literal(CHANNEL_NAMES[i]), btn -> {
+                boolean power = this.menu.isPowered();
+                PacketDistributor.sendToServer(new ServerboundLcdTvUpdate(
+                        this.menu.getBlockEntity().getBlockPos(), power, channelIndex));
+            }).bounds(startX + 8 + i * 40, startY + 45, 36, 20).build());
+        }
+    }
+
+    private Component getPowerLabel() {
+        return this.menu.isPowered()
+                ? Component.literal("Power: ON")
+                : Component.literal("Power: OFF");
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
+        // Dark background panel
+        graphics.fill(this.leftPos, this.topPos, this.leftPos + this.imageWidth, this.topPos + this.imageHeight, 0xC0101010);
+        // Border
+        graphics.fill(this.leftPos, this.topPos, this.leftPos + this.imageWidth, this.topPos + 1, 0xFF555555);
+        graphics.fill(this.leftPos, this.topPos + this.imageHeight - 1, this.leftPos + this.imageWidth, this.topPos + this.imageHeight, 0xFF555555);
+        graphics.fill(this.leftPos, this.topPos, this.leftPos + 1, this.topPos + this.imageHeight, 0xFF555555);
+        graphics.fill(this.leftPos + this.imageWidth - 1, this.topPos, this.leftPos + this.imageWidth, this.topPos + this.imageHeight, 0xFF555555);
+
+        // Channel indicator bar - show color of active channel if powered
+        if (this.menu.isPowered()) {
+            int ch = this.menu.getChannel();
+            int indicatorColor = CHANNEL_COLORS[ch];
+            graphics.fill(this.leftPos + 8, this.topPos + 75, this.leftPos + this.imageWidth - 8, this.topPos + 90, indicatorColor);
+            graphics.drawString(this.font,
+                    Component.literal("Channel " + (ch + 1) + " - ON"),
+                    this.leftPos + 14, this.topPos + 79, 0xFF000000, false);
+        } else {
+            graphics.fill(this.leftPos + 8, this.topPos + 75, this.leftPos + this.imageWidth - 8, this.topPos + 90, 0xFF222222);
+            graphics.drawString(this.font,
+                    Component.literal("TV is OFF"),
+                    this.leftPos + 14, this.topPos + 79, 0xFF888888, false);
+        }
+    }
+
+    @Override
+    protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
+        graphics.drawString(this.font, this.title, (this.imageWidth - this.font.width(this.title)) / 2, -10, 0xFFAAAAAA, false);
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        this.renderBackground(graphics, mouseX, mouseY, delta);
+        super.render(graphics, mouseX, mouseY, delta);
+        this.renderTooltip(graphics, mouseX, mouseY);
+    }
+}
